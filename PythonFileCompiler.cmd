bcc9f8af746fd598895455b19dcef7ed00dbb894
JG <it@hgw-tornesch.de>
2024-09-10 14:59:39 +0200

    Added Python2Exe compiler script

---
+++ PythonFileCompiler.cmd
@@ -0,0 +1,24 @@
+@echo off
+set s=%1
+set en=%2
+set e=%en%.exe
+set p=%3
+
+echo Compiling exe file. . .
+echo.
+pyinstaller.exe --onefile "%p%\%s%.py"
+echo.
+robocopy dist . * /MOV
+if exist %e% del %e%
+ren %s%.exe %e%
+echo Compilation finished.
+echo.
+echo Deleting build files. . .
+del /q %s%.spec
+rd /s /q build
+rd /q dist
+echo Cleanup finished.
+echo.
+echo Exe file can now be used.
+pause
+exit
---
+++ iPhonePhotoSortOutCompiler.cmd
@@ -0,0 +1,6 @@
+@echo off
+set s=iphone_photo_sortout
+set n="iPhone Photo Sortout"
+set p="."
+
+start PythonFileCompiler.cmd %s% %n% %p%
