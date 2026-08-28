edf91d2348e2664f3cf08b86b4e6af6f6ebfa173
Rsge <rsge@web.de>
2026-08-28 14:24:55 +0200

    Change PS naming convention

--- ChangeUserProfileName.ps1
+++ ChangeUserProfileName.ps1
@@ -1,14 +1,15 @@
+# Base user directory
 $UsersDir = "C:\Users\"
 
 # Get current user dir name.
-while ($True) {
-    $PrevName = Read-Host -Prompt "Current name of user's directory"
-    if (!$PrevName) {
+while ($true) {
+    $prevName = Read-Host -Prompt "Current name of user's directory"
+    if (!$prevName) {
         Write-Output "Aborting..."
         return
     }
     try {
-        $UserFolder = Get-Item -Path "$UsersDir$PrevName" -ErrorAction Stop
+        $userFolder = Get-Item -Path "$UsersDir$prevName" -ErrorAction Stop
         break
     } catch {
         Write-Warning $_.Exception.Message
@@ -16,15 +17,16 @@ while ($True) {
 }
 
 # Get new user name.
-while ($True) {
-    $PrincipalName = Read-Host -Prompt "New principal name of user"
-    if (!$PrincipalName) {
+while ($true) {
+    $principalName = Read-Host -Prompt "New principal name of user"
+    $principalName = $principalName -replace '@.*$' -replace '^.*\\'
+    if (!$principalName) {
         Write-Output "Aborting..."
         return
     }
     try {
-        $Id = New-Object System.Security.Principal.NTAccount($PrincipalName) -ErrorAction Stop
-        $Sid = $Id.Translate([System.Security.Principal.SecurityIdentifier]).Value
+        $id = New-Object System.Security.Principal.NTAccount($principalName) -ErrorAction Stop
+        $sID = $id.Translate([System.Security.Principal.Securityidentifier]).Value
         break
     } catch {
         Write-Warning "This user can't be found:"
@@ -33,26 +35,27 @@ while ($True) {
 }
 
 # Check if logged in as this user.
-$CId = New-Object System.Security.Principal.NTAccount($env:USERNAME)
-$CSid = $CId.Translate([System.Security.Principal.SecurityIdentifier]).Value
-if ($Sid.Equals($CSid)) {
+$cID = New-Object System.Security.Principal.NTAccount($env:USERNAME)
+$csID = $cID.Translate([System.Security.Principal.Securityidentifier]).Value
+if ($sID.Equals($csID)) {
     Write-Error "
     You are currently logged in as this user. You can't rename a user you are logged in as.
     Please change to an admin user different from the user to rename."
+    pause
     return
 }
 
 # Rename folder.
 Write-Output "Renaming folder..."
 try {
-    Rename-Item -Path "$UserFolder" -NewName "$PrincipalName" -ErrorAction Stop
+    Rename-Item -Path "$userFolder" -NewName "$principalName" -ErrorAction Stop
 } catch {
     Write-Error "Couldn't rename. Did you run this as admin?"
 }
 
 # Change registry entry.
 Write-Output "Changing registry..."
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\$Sid" -Name ProfileImagePath -Value "$UsersDir$PrincipalName"
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\$sID" -Name ProfileImagePath -Value "$UsersDir$principalName"
 
 Write-Output "Done."
 Pause
